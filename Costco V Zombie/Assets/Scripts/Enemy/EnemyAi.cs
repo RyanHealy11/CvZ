2e9e7af93024d95e94ca449ce3a96306c65d0636
RyanHealy11 <42445767+RyanHealy11@users.noreply.github.com>
2020-06-29 17:33:36 -0700

    basic combat

---
+++ Costco V Zombie/Assets/Scripts/Enemy/EnemyAi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public GameObject Target;
+
+    public float Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(Target.transform.position);
+
+        gameObject.transform.position += (gameObject.transform.forward * Speed * Time.deltaTime);
+    }
+}
---
+++ Costco V Zombie/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int Health = 1;
+
+    private void Update()
+    {
+        if (Health <= 0) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bad") 
+        {
+            Health--;
+        }
+    }
+}
