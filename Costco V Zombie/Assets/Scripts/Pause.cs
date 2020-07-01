0483ddba63ac49bc48b457037b75e77aa2ae67f5
RyanHealy11 <42445767+RyanHealy11@users.noreply.github.com>
2020-06-30 17:05:19 -0700

    added basic features

---
+++ Costco V Zombie/Assets/Scripts/Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public Canvas GameCanvas;
+    public Canvas PauseCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            GameCanvas.gameObject.SetActive(!GameCanvas.gameObject.activeSelf);
+            PauseCanvas.gameObject.SetActive(!PauseCanvas.gameObject.activeSelf);
+        }
+               
+    }
+}
