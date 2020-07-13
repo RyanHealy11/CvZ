392908388992ea400ae0cb63d8936ad305f9eb02
RyanHealy11 <42445767+RyanHealy11@users.noreply.github.com>
2020-07-13 15:24:11 -0700

    update
    score and respawning zombs

--- Costco V Zombie/Assets/Scripts/Enemy/EnemyAi.cs
+++ Costco V Zombie/Assets/Scripts/Enemy/EnemyAi.cs
@@ -6,11 +6,18 @@ public class EnemyAi : MonoBehaviour
 {
     public GameObject Target;
 
+    public int Health = 1;
+    private int StartHealth;
+    public bool Dead = false;
+
+    public Vector3 SpawnCorner1;
+    public Vector3 SpawnCorner2;
+
     public float Speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartHealth = Health;
     }
 
     // Update is called once per frame
@@ -19,5 +26,20 @@ public class EnemyAi : MonoBehaviour
         gameObject.transform.LookAt(Target.transform.position);
 
         gameObject.transform.position += (gameObject.transform.forward * Speed * Time.deltaTime);
+        if (Health <= 0) 
+        {
+            Dead = true;
+        }
+        if (Dead) 
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn() 
+    {
+        gameObject.transform.position = new Vector3(Random.Range(SpawnCorner1.x, SpawnCorner2.x), transform.position.y, Random.Range(SpawnCorner1.z, SpawnCorner2.z));
+        Dead = false;
+        Health = StartHealth;
     }
 }
---
+++ Costco V Zombie/Assets/Scripts/Player/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Camera camera;
+    public Score score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Debug.Log("FIRE!!");
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit)) 
+            {
+                if (hit.transform.gameObject.TryGetComponent(out EnemyAi bob))
+                {
+                    bob.Health--;
+                    if (bob.Health <= 0) 
+                    {
+                        score.curScore++;                    
+                    }
+                }                                
+            }
+        }        
+    }
+}
